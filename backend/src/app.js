59e6240168ebc2f9a008b8824932209ab2b1ba89
het gajjar <hetg69774@gmail.com>
2026-05-03 10:29:08 +0530

    small changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,34 +2,36 @@ import express from 'express';
 import connectDB from './config/database.js';
 import cookie from 'cookie-parser';
 import AuthRouter from './routes/Auth.routes.js';
-import test from './services/Ai.service.js';
 import cors from 'cors';
 import morgan from 'morgan';
 import chatRouter from './routes/chat.routes.js';
 
-
-
-
-
-
 const app = express();
+
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use(cors(
-    {
-        origin: "https://query-nova-ai.vercel.app",
-        credentials: true,
-    }
-))
+const allowedOrigins = [
+  "http://localhost:5174",
+  "https://query-nova-ai.vercel.app"
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  },
+  credentials: true
+}));
+
+app.options("*", cors());
 
 app.use(cookie());
+
 app.use('/api/auth', AuthRouter);
 app.use('/api/chats', chatRouter);
-
-
-
-
 
 export default app;
